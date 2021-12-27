f950659b99cfdc000b8e7b820bd3dbaf0c1d8b3e
Mr-Walrus <anokhin.danilka-2001@yandex.ru>
2021-12-27 11:38:46 +0300

    Initial commit

---
+++ laba_05_02_03/laba_05_02_03.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include "stringStream.h"
+using namespace std;
+
+
+
+void taskOne()
+{
+	int menu;
+	while (true)
+	{
+		cout << "\tStandard stream task" << endl
+			<< "  1. Standard stream task" << endl
+			<< "  2. String   stream task" << endl
+			<< "  0. Back" << endl
+			<< "-> ";
+		cin >> menu;
+
+		while (menu < 0 || menu > 2 || cin.fail()) {
+			cin.clear();
+			cin.ignore(32767, '\n');
+			cout << "Non-existent menu case. Re-enter" << endl;
+			cin >> menu;
+		}
+		switch (menu)
+		{
+		case 1: {  break; }
+		case 2: {  break; }
+		case 0: { return; }
+		}
+	}
+}
+
+void taskTwo()
+{
+	cout << "\tString stream task:" << endl << endl;
+	findTwoDigit();
+	cout << endl;
+}
+
+void menu()
+{
+	int menu;
+	while (true)
+	{
+		cout << "\tMain menu" << endl
+			<< "1. Standard stream task" << endl
+			<< "2. String   stream task" << endl
+			<< "0. Exit" << endl
+			<< "-> ";
+		cin >> menu;
+
+		while (menu < 0 || menu > 2 || cin.fail()) {
+			cin.clear();
+			cin.ignore(32767, '\n');
+			cout << "Non-existent menu case. Re-enter" << endl;
+			cin >> menu;
+		}
+		switch (menu)
+		{
+		case 1: { taskOne(); break; }
+		case 2: { taskTwo(); break; }
+		case 0: { return; }
+		}
+	}
+}
+
+int main()
+{
+	try { 
+		menu(); 
+	}
+	catch (Exception& exception) {
+		cout << "Exception occurred : " << exception.what() << endl;
+	}
+	catch (exception& exception) {
+		cout << "Some other exception occurred : " << exception.what() << endl;
+	}
+}
